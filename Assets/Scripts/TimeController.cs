87c3a8b1fd5cafe4852dd50c540ea396815a9a27
Luke Hirst <521480@chrome.escg.ac.uk>
2023-03-24 17:57:40 +0000

    Working TimeController!

--- Assets/Scripts/TimeController.cs
+++ Assets/Scripts/TimeController.cs
@@ -7,7 +7,7 @@ using TMPro;
 public class TimeController : MonoBehaviour
 {
     [SerializeField]
-    private float TimeMultipler;
+    private float timeMultipler;
 
     [SerializeField]
     private float startHour;
@@ -23,6 +23,24 @@ public class TimeController : MonoBehaviour
 
     [SerializeField]
     private float sunsetHour;
+
+    [SerializeField]
+    private Color dayAmbientLight;
+
+    [SerializeField]
+    private Color nightAmbientLight;
+
+    [SerializeField]
+    private AnimationCurve lightChangeCurve;
+
+    [SerializeField]
+    private float maxSunLightIntensity;
+
+    [SerializeField]
+    private Light moonLight;
+
+    [SerializeField]
+    private float maxMoonLightIntensity;
 
     private DateTime currentTime;
 
@@ -42,20 +60,69 @@ public class TimeController : MonoBehaviour
     void Update()
     {
         UpdateTimeOfDay();
+        RotateOrbits();
+        UpdateLightSettings();
     }
 
     private void UpdateTimeOfDay()
     {
-        currentTime = currentTime.AddSeconds(Time.deltaTime * TimeMultipler);
+        currentTime = currentTime.AddSeconds(Time.deltaTime * timeMultipler);
 
         if (timeText != null)
         {
             timeText.text = currentTime.ToString("HH:mm");
         }
     }
+    private void RotateOrbits()
+    {
+        float sunLightRotation;
+        float moonLightRotation;
+
+        if (currentTime.TimeOfDay > sunriseTime && currentTime.TimeOfDay < sunsetTime)
+        {
+            TimeSpan sunriseToSunsetDuration = CalculateTimeDifference(sunriseTime, sunsetTime);
+
+            TimeSpan timeSinceSunrise = CalculateTimeDifference(sunriseTime, currentTime.TimeOfDay);
+
+            double percentage = timeSinceSunrise.TotalMinutes / sunriseToSunsetDuration.TotalMinutes;
+
+            sunLightRotation = Mathf.Lerp(0, 180, (float)percentage);
+
+            moonLightRotation = Mathf.Lerp(-180, 0, (float)percentage);
+        }
+        else
+        {
+            TimeSpan sunsetToSunriseDuration = CalculateTimeDifference(sunsetTime, sunriseTime);
+
+            TimeSpan timeSinceSunset = CalculateTimeDifference(sunsetTime, currentTime.TimeOfDay);
+
+            double percentage = timeSinceSunset.TotalMinutes / sunsetToSunriseDuration.TotalMinutes;
+
+            sunLightRotation = Mathf.Lerp(180, 360, (float)percentage);
+
+            moonLightRotation = Mathf.Lerp(-360, -180, (float)percentage);
+        }
+
+        sunLight.transform.rotation = Quaternion.AngleAxis(sunLightRotation, Vector3.right);
+        moonLight.transform.rotation = Quaternion.AngleAxis(moonLightRotation, Vector3.right);
+    }
+    private void UpdateLightSettings()
+    {
+        float dotProduct = Vector3.Dot(sunLight.transform.forward, Vector3.down);
+        sunLight.intensity = Mathf.Lerp(0, maxSunLightIntensity, lightChangeCurve.Evaluate(dotProduct));
+        moonLight.intensity = Mathf.Lerp(maxMoonLightIntensity, 0, lightChangeCurve.Evaluate(dotProduct));
+        RenderSettings.ambientLight = Color.Lerp(nightAmbientLight, dayAmbientLight, lightChangeCurve.Evaluate(dotProduct));
+    }
 
     private TimeSpan CalculateTimeDifference(TimeSpan fromTime, TimeSpan toTime)
     {
         TimeSpan difference = toTime - fromTime;
+
+        if (difference.TotalSeconds < 0)
+        {
+            difference += TimeSpan.FromHours(24);
+        }
+
+        return difference;
     }
 }
