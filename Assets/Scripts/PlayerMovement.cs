e1eaad4c7fe3ac3c87903234c5b0e35a40bb1109
Luke Hirst <521480@chrome.escg.ac.uk>
2023-03-31 09:41:12 +0100

    Tweaked the movement to work

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -11,18 +11,15 @@ public class PlayerMovement : MonoBehaviour
     private float runSpeed = 12f;
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
+    private string currentState;
+    Rigidbody Player;
 
-    //Animation states
-    const string PLAYER_IDLE = "Player_idle";
-    const string PLAYER_WALK = "Player_walk";
-    const string PLAYER_RUN = "Player_run";
-
-    void Start
+    void Start()
     {
         anim = GetComponent<Animator>();
         controller = GetComponent<CharacterController>();
+        Player = GetComponent<Rigidbody>();
     }
-
     void Update()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
@@ -31,36 +28,27 @@ public class PlayerMovement : MonoBehaviour
 
         if (direction.magnitude >= 0.01f)
         {
-        ChangeAnimationState(PLAYER_WALK);
+            anim.SetBool("isWalking", true);
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
         }
-        else if (direction.magnitude <= 0.01f)
-    {
-        ChangeAnimationState(PLAYER_IDLE);
-    }
+        else if (direction.magnitude < 0.01f)
+        {
+            anim.SetBool("isWalking", false);
+        }
 
-    if (Input.GetKey(KeyCode.LeftShift) || (Input.GetKey(KeyCode.RightShift)))
-    {
-        runSpeed = speed * 2;
-        ChangeAnimationState(PLAYER_RUN);
-        Debug.Log("Target is running at: " + runSpeed);
-    }
-    else
-    {
-        ChangeAnimationState(PLAYER_WALK);
-    }
-
-    void ChangeAnimationState(string newState)
-    {
-        if (currentState == newState) return;
-
-        anim.Play(newState);
-
-        currentState = newState;
-    }
+        if (Input.GetKey(KeyCode.LeftShift) || (Input.GetKey(KeyCode.RightShift)))
+        {
+            runSpeed = speed * 2;
+            anim.SetBool("isRunning", true);
+            Debug.Log("Target is running at: " + runSpeed);
+        }
+        else
+        {
+            anim.SetBool("isRunning", false);
+        }
     }
 }
