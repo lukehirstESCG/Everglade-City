2311811478fb3bd7cb7d90f5fca9a709654d9e14
Luke Hirst <521480@chrome.escg.ac.uk>
2023-04-03 16:35:21 +0100

    Added in a traffic light system

---
+++ Assets/Scripts/TrafficLight.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using UnityEngine;
+
+public class TrafficLight : MonoBehaviour
+{
+    public GameObject redLight;
+    public GameObject yellowLight;
+    public GameObject greenLight;
+
+    private void Start()
+    {
+        StartCoroutine(TrafficLightCoroutine());
+        redLight.SetActive(true);
+    }
+
+    IEnumerator TrafficLightCoroutine()
+    {
+        while (true)
+        {
+            if (greenLight == true)
+            {
+                greenLight.SetActive(false);
+                yellowLight.SetActive(true);
+                yield return new WaitForSeconds(10f);
+            }
+            if (yellowLight == true)
+            {
+                yellowLight.SetActive(false);
+                redLight.SetActive(true);
+                yield return new WaitForSeconds(2f);
+            }
+            if (redLight == true)
+            {
+                yellowLight.SetActive(true);
+                yield return new WaitForSeconds(10f);
+            }
+            if (redLight && yellowLight == true)
+            {
+                redLight.SetActive(false);
+                yellowLight.SetActive(false);
+                greenLight.SetActive(true);
+                yield return new WaitForSeconds(10f);
+            }
+            if (greenLight == true)
+            {
+                yellowLight.SetActive(false);
+                greenLight.SetActive(false);
+                redLight.SetActive(false);
+                yield return new WaitForSeconds(1f);
+            }
+
+           }
+        }
+    }
