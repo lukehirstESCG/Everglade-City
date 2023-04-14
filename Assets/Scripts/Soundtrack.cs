36a600b4806104fb2d3810906fbefb5d977da102
Luke Hirst <521480@chrome.escg.ac.uk>
2023-04-14 16:48:19 +0100

    Started adding a soundtrack.

--- Assets/Scripts/Soundtrack.cs
+++ Assets/Scripts/Soundtrack.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class Soundtrack : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private string currentTrackName = "";
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        StartCoroutine(MusicList());
+    }
+    IEnumerator MusicList()
+    {
+        while (true)
+        {
+            if (Input.GetKey(KeyCode.R))
+            {
+                {
+                    if (!string.IsNullOrEmpty(currentTrackName))
+                    {
+                        FindObjectOfType<AudioManager>().Stop(currentTrackName);
+                    }
+                }
+                currentTrackName = "Track1";
+                FindObjectOfType<AudioManager>().Play("Track1");
+                yield return new WaitForSeconds(155.4f);
+                FindObjectOfType<AudioManager>().Stop("Track1");
+
+                currentTrackName = "Track2";
+                FindObjectOfType<AudioManager>().Play("Track2");
+                yield return new WaitForSeconds(192.6f);
+                FindObjectOfType<AudioManager>().Stop("Track2");
+            }
+                
+        }
     }
 }
