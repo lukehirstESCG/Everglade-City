eba25fb55e5c7151d542bb889611b39065e2ade6
Luke Hirst <521480@chrome.escg.ac.uk>
2023-04-23 18:40:13 +0100

    Began decorating the Waterfront.

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     [SerializeField] private Transform target;
-    [SerializeField] private float offsetX, offsetZ;
+
+    [SerializeField] private float offsetX, offsetZ, offsetY;
     [SerializeField] private float LerpSpeed;
 
     private void Update()
     {
+        MinimapTravel();;
+    }
+    private void MinimapTravel()
+    {
+        // This tells the camera where to travel to with the player.
         transform.position = Vector3.Lerp(transform.position,
-            new Vector3(target.position.x + offsetX, transform.position.y, target.position.z + offsetZ), LerpSpeed);
+        new Vector3(target.position.x + offsetX, transform.position.y + offsetY, target.position.z + offsetZ), LerpSpeed);
     }
 }
---
+++ Assets/Scripts/ValenciaislandLights.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ValenciaislandLights : MonoBehaviour
+{
+    public GameObject V;
+    public GameObject A;
+    public GameObject L;
+    public GameObject E;
+    public GameObject N;
+    public GameObject C;
+    public GameObject I;
+    public GameObject A2;
+
+    private void Start()
+    {
+        V.SetActive(true);
+        StartCoroutine(ValenciaIslandLightShow());
+    }
+    IEnumerator ValenciaIslandLightShow()
+    {
+        while (true)
+        {
+            if (V == true)
+            {
+                V.SetActive(false);
+                A.SetActive(true);
+                yield return new WaitForSeconds(1);
+            }
+            if (A == true)
+            {
+                A.SetActive(false);
+                L.SetActive(true);
+                yield return new WaitForSeconds(1);
+            }
+            if (L == true)
+            {
+                L.SetActive(false);
+                E.SetActive(true);
+                yield return new WaitForSeconds(1);
+            }
+            if (E == true)
+            {
+                E.SetActive(false);
+                N.SetActive(true);
+                yield return new WaitForSeconds(1);
+            }
+            if (N == true)
+            {
+                N.SetActive(false);
+                C.SetActive(true);
+                yield return new WaitForSeconds(1);
+            }
+            if (C == true)
+            {
+                C.SetActive(false);
+                I.SetActive(true);
+                yield return new WaitForSeconds(1);
+            }
+            if (I == true)
+            {
+                I.SetActive(false);
+                A2.SetActive(true);
+                yield return new WaitForSeconds(1);
+            }
+            if (A2 == true)
+            {
+                V.SetActive(true);
+                A.SetActive(true);
+                L.SetActive(true);
+                E.SetActive(true);
+                N.SetActive(true);
+                C.SetActive(true);
+                I.SetActive(true);
+                yield return new WaitForSeconds(1);
+            }
+            if (V && A && L && E && N && C && I && A == true)
+            {
+                V.SetActive(false);
+                A.SetActive(false);
+                L.SetActive(false);
+                E.SetActive(false);
+                N.SetActive(false);
+                C.SetActive(false);
+                I.SetActive(false);
+                yield return new WaitForSeconds(1);
+            }
+        }
+    }
+}
